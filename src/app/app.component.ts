5f245e8be667fdc9f7004656b279fc8718f58ae1
François Nys <francois.nys@web-atrio.com>
2023-06-30 09:39:38 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { likedinService } from './linkedin-service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  public textToCheck:any = ""
+  public hashtagList:string[] = []
+  public followerCount = 0
+
+  constructor(private service: likedinService){}
+
+  ngOnInit(): void {
+
+  }
+
+  checkValue() {
+
+    let textWords: string[] = this.textToCheck.split(/\s+/)
+    this.hashtagList = []
+
+    textWords.forEach((element) => {
+      if(element.indexOf("#") > -1 && element != "##") {
+        this.hashtagList.push(element)
+      }
+    });;
+
+    this.checkFollowerCount();
+  }
+
+  reset() {
+    this.textToCheck = ""
+    this.hashtagList = []
+    this.followerCount = 0
+  }
+
+  checkFollowerCount() {
+    this.hashtagList.forEach( x => {
+      //appel à linkedin.
+    })
+  }
+
+
+
+}
---
+++ src/app/linkedin-service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+    providedIn: 'root'
+})
+export class likedinService {
+
+    testKiwi() {
+      return "hello"
+    }
+
+
+
+}
